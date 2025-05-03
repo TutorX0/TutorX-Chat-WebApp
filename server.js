5d0dc94dc27eb78ff6b73aeb19c35941b141361f
angkushsahu <angkysahu2502@gmail.com>
2025-05-03 14:03:51 +0530

    refactor: added cors configuration and api prefixes"
    1. Added cors for all origins for testing
    2. Added /api prefixes before each api route

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
@@ -10,18 +11,18 @@ const Chat = require("./models/chatModel");
 dotenv.config();
 connectDB();
 
-
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/chat", chatRoutes);
-app.use("/", webhookRoutes);
-app.use("/",userRoutes);
-// Webhook verification endpoint
+app.use(cors({ origin: true, credentials: true }));
 
+app.use("/api/chat", chatRoutes);
+app.use("/api", webhookRoutes);
+app.use("/api", userRoutes);
+// Webhook verification endpoint
 
 // Start your server
 app.listen(3000, () => {
-    console.log("Webhook server is running on port 3000");
+	console.log("Webhook server is running on port 3000");
 });
